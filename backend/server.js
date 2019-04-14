6ae9d8b38fbd01cb84f0b7187861ae291dc76ed7
Kevin Boronka <kboronka@gmail.com>
2019-04-14 10:42:07 -0400

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,87 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+import Issue from './models/Issue';
+
+const app = express();
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/issues');
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB connection established successfully!')
+});
+
+router.route('/issues').get((req, res) => {
+  Issue.find((err, issues) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(issues);
+    }
+  });
+});
+
+router.route('/issues/:id').get((req, res) => {
+  Issue.findById(req.params.id, (err, issue) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(issue);
+    }
+  });
+});
+
+router.route('/issues/add').post((req, res) => {
+  let issue = new Issue(req.body);
+  issue.save()
+    .then(issue => {
+      res.status(200).json({ 'issue': 'Added successfully' });
+    })
+    .catch(err => {
+      res.status(400).send('Failed to create a new record');
+    });
+});
+
+router.route('/issues/update/:id').post((req, res) => {
+  console.log(req.params.id);
+  Issue.findById(req.params.id, (err, issue) => {
+    if (!issue || err) {
+      res.status(400).send('issue not found');
+    } else {
+      issue.title = req.body.title;
+      issue.responsible = req.body.responsible;
+      issue.description = req.body.description;
+      issue.severity = req.body.severity;
+      issue.status = req.body.status;
+
+      issue.save()
+        .then(issue => {
+          res.status(200).json({ 'issue': 'Updated successfully' });
+        })
+        .catch(err => {
+          res.status(400).send('Failed to update record');
+        });
+    }
+  });
+});
+
+router.route('/issues/delete/:id').get((req, res) => {
+  Issue.findByIdAndRemove(req.params.id, (err, issue) => {
+    if (!err) {
+      res.status(200).json({ 'issue': 'Removed successfully' });
+    } else {
+      res.status(400).send('Failed to remove record');
+    }
+  });
+});
+
+app.use('/', router);
+
+app.listen(4000, () => console.log("listening on port 4000"));
