a26d3d73f266da966a7212cd0532a30ad62d6354
Kevin Boronka <kboronka@gmail.com>
2019-04-14 17:44:48 -0400

    got the home page styled

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-list',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log('constructor');
+    document.body.className = "splash";
   }
 
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngOnDestory() {
+    console.log('ngOnDestory');
+  }
 }
