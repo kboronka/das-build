ead86b894c07d20fb9a9c0f51ef6dfad4c0d8f27
Kevin Boronka <kboronka@gmail.com>
2019-04-21 11:34:58 -0400

    path cleanup

--- backend/src/app/components/profile/profile.component.ts
+++ backend/src/app/components/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService, UserAuthInfo } from '../../services/users.service';
-import { User } from '../../../../../backend/models/user';
+import { User } from '../../../../models/user';
 
 interface ProfileResponce {
   user: User;
